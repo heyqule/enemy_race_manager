358ea6454efb3bfefa2b30d1606b9c7e75fadc28
heyqule <hq.jfax@gmail.com>
2021-11-25 18:18:53 -0500

    1.10.3 commit
    Fix:
    https://github.com/heyqule/enemy_race_manager/issues/4
    
    Change on time based squads

--- data.lua
+++ data.lua
@@ -5,12 +5,10 @@ require "prototypes.extend-bitters"
 require "prototypes.extend-spawners"
 
 
-if settings.startup['enemyracemanager-enable-bitters'].value == true then
-    require "prototypes.base-units.defender"
-    require "prototypes.base-units.destroyer"
-    require "prototypes.base-units.distractor"
-    require "prototypes.base-units.construction"
-    require "prototypes.base-units.logistic"
+require "prototypes.base-units.defender"
+require "prototypes.base-units.destroyer"
+require "prototypes.base-units.distractor"
+require "prototypes.base-units.construction"
+require "prototypes.base-units.logistic"
 
-    require "prototypes.base-spawner.roboport"
-end
+require "prototypes.base-spawner.roboport"
