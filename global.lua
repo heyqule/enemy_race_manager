heyqule/erm_zerg

- Refactor to use force name instead of race_name for custom data.

global.lua
@@ -6,7 +6,7 @@
 
 -- Globals within the mod
 -- Must be same as the name in info.json and it must be start with erm_ prefix.
-MOD_NAME = "erm_zerg"
+MOD_NAME = "enemy_erm_zerg"
 -- Must be prefix with "enemy_"
 FORCE_NAME = "enemy_erm_zerg"
 

heyqule/enemy_race_manager

global.lua
@@ -5,7 +5,7 @@
 ---
 
 -- FOR VANILLA ENEMIES / enemy forces.
-MOD_NAME = "erm_vanilla"
+MOD_NAME = "enemy"
 FORCE_NAME = "enemy"
 
 --- [ MAKE SURE TO TURN THESE FLAG OFF BEFORE GO PROD ]